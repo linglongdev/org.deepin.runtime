4a8eab928a16c727eb01c6c4b5b89cefaa0c4376
myml <wurongjie@deepin.org>
2025-02-13 15:11:05 +0800

    feat: add webengine

--- daily.bash
+++ daily.bash
@@ -10,4 +10,5 @@ cd vscode-linglong
 for file in linglong.yaml arm64/linglong.yaml loong64/linglong.yaml sw64/linglong.yaml; do
     sed -i "s#version: 25.2.0.*#version: 25.2.0.$t#" ../$file
     go run ./src/tools ../$file
+    sed -i "s#id: org.deepin.runtime.dtk\$#id: org.deepin.runtime.webengine#" ../$file
 done
